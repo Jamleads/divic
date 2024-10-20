8cca290d7932cc260e6a57c55581ec199cf9d136
jamleads <ogunyemiadetunji17@gmail.com>
2024-10-20 11:45:08 +0100

    dev payment issue

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,18 +1,38 @@
 import Footer from "./Footer";
 import Nav from "./Nav";
 import { Outlet } from "react-router-dom";
+import { BiSolidError } from "react-icons/bi";
 
 const Layout = () => {
+  const devpayment = null;
   return (
-    <>
-      <Nav />
+    <div className={` ${!devpayment ? "h-[100vh] overflow-hidden" : ""}`}>
+      <div className="modal md:w-[35%] w-[95%] py-20 px-5 flex flex-col items-center justify-center gap-20 rounded-2xl">
+        <div className="flex items-center justify-center gap-5 text-red-700 text-5xl">
+          <BiSolidError />
+          Error 403
+        </div>
+        <div>
+          <p className="text-2xl text-center">Access restricted.</p>
+          <p className="text-center">
+            The developer responsible for building this website has not received
+            payment for their services.
+          </p>
+        </div>
+      </div>
 
-      <>
-        <Outlet />
-      </>
+      {!devpayment && <div className="modal-backdrop"></div>}
 
-      <Footer />
-    </>
+      <div>
+        <Nav />
+
+        <>
+          <Outlet />
+        </>
+
+        <Footer />
+      </div>
+    </div>
   );
 };
 
